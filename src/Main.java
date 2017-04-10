afdda46fe74e81a076f1d6321ffec084918c7910
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2017-04-11 00:35:29 +0400

    Main method is separated

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception{
-
-        stage.show();
-    }
-
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(Runner.class, args);
     }
 }
