e0c3162da2bc4013eccd3d18950eb25bc00a44ff
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2017-04-11 00:52:31 +0400

    Runner is changed a bit

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,13 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
